5b1f90f096b40b30c176fc9dc395191b82ae6e1c
Muhammad Shekh Yaseen <casemng@gmail.com>
2026-01-16 01:13:28 +0300

    Mapping

--- DataIntegration/DataSource/DummyJson/DTOs/DummyJsonProductDto.cs
+++ DataIntegration/DataSource/DummyJson/DTOs/DummyJsonProductDto.cs
@@ -1,4 +1,12 @@
 namespace DataIntegration.DataSource.DummyJson.DTOs
-{ 
-
+{
+    public class DummyJsonProductDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public DateTime LastModified { get; set; }
+    }
 }
--- DataIntegration/MappingProfiles/DummyJson/DummyJsonProductMapper.cs
+++ DataIntegration/MappingProfiles/DummyJson/DummyJsonProductMapper.cs
@@ -1,1 +1,13 @@
-// placeholder
+using DataIntegration.DataSource.DummyJson.DTOs;
+using DataIntegration.Models.Abstractions;
+using DataIntegration.Models.Payloads;
+
+namespace DataIntegration.MappingProfiles.DummyJson
+{
+    public class DummyJsonProductMapper : IMapper<DummyJsonProductDto, ProductPayload>
+    {
+        public ProductPayload Map(DummyJsonProductDto s) =>
+            new(s.Id.ToString(), s.Title, s.Description,
+                [new("Piece", s.Price)], s.Category);
+    }
+}
